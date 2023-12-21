75a567fc6687535473c08108f36fe7ed4cc758e9
malikbae <a.malikbaehaqi@gmail.com>
2023-12-21 20:58:03 +0700

    dashboard

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import Konten from "../components/Konten";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Sidebar />
+      <Konten />
+    </div>
+  );
+};
+
+export default Dashboard;
